77fe54cabf1a201861f42d3d82101a810dfd3d4a
clinturbin <clinturbin@gmail.com>
2018-09-25 14:48:01 -0400

    Created New Quiz Screen

--- src/user-dashboard.js
+++ src/user-dashboard.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import AppHeader from './app-header';
 import './stylesheets/user-dashboard.css';
+import { Link } from 'react-router-dom';
 
 let UserDashboardScreen = (props) => (
     <div>
         <AppHeader />
         <div className='user-dashboard'>
             <h3>Greetings: {props.username}</h3>
-            <div className='dashboard-link'>Create Quiz</div>
+            <Link to='/quizzes/new'>
+                <div className='dashboard-link'>Create Quiz</div>
+            </Link>
             <div className='dashboard-link'>My Quizzes</div>
         </div>
     </div>
