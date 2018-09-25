f28190e41317c672c770431cd0f91de01b835aad
Mmsimpson <Marshall1044@gmail.com>
2018-09-25 14:55:24 -0400

    merge for github repo reorganization

--- src/queries.js
+++ src/queries.js
@@ -8,6 +8,18 @@ let usernameLogin = (username, password) => {
     AND password = '` + password + `';`)
 }
 
+let checkUser = (username) => {
+    return db.query(`select username
+        FROM users
+        WHERE username = '` + username + `';`);
+}
+
+let createUser = (userForm) => {            
+    return db.one(`INSERT INTO users (username, password, email) values    
+    ('` + userForm.username + `', '` + userForm.password + `', '` + userForm.email + `')
+    RETURNING *;`);  
+} 
+
 exports.usernameLogin = usernameLogin;
-
-
+exports.createUser = createUser;
+exports.checkUser = checkUser;
