dbe9c9c045ce2d30037b8c3104c1ea58b04b71bb
Mmsimpson <Marshall1044@gmail.com>
2018-09-26 10:05:43 -0400

    users can now be added and stored in the database

--- backend/queries.js
+++ backend/queries.js
@@ -1,25 +1,20 @@
 const pg = require('pg-promise')();
-const db = pg('postgres://marshallsimpson@localhost:5432/stump') 
+const db = pg('postgres://marshallsimpson@localhost:5432/stump'); 
 
 let usernameLogin = (username, password) => {
+    console.log(username, password)
     return db.one(`select username, password, id
     FROM users
     WHERE username = '` + username + `'
     AND password = '` + password + `';`)
 }
 
-let checkUser = (username) => {
-    return db.query(`select username
-        FROM users
-        WHERE username = '` + username + `';`);
-}
-
-let createUser = (userForm) => {            
+let createUser = (username, password, email) => {            
     return db.one(`INSERT INTO users (username, password, email) values    
-    ('` + userForm.username + `', '` + userForm.password + `', '` + userForm.email + `')
+    ('` + username + `', '` + password + `', '` + email + `')
     RETURNING *;`);  
 } 
 
 exports.usernameLogin = usernameLogin;
 exports.createUser = createUser;
-exports.checkUser = checkUser;
+
--- backend/server.js
+++ backend/server.js
@@ -9,75 +9,29 @@ ex.use(cors());
 ex.use(jsonParser);
 
 let newUser = (req, res) => {
-    let userform = req.body;
-    let userNameTaken = {response: "Username Taken"};
-    dbq.checkUser(userForm.username)
-        .then(results => {
-            if(results === undefined || results.length == 0) {
-                dbq.createUser(userForm)
-                    .then(results => {
-                        res.send(results);
-                    })
-            } else {
-                res.send(userNameTaken);
-            }
-        })
-}
-
-let validateToken = (req, res) => {
-    let responseObject = {response: null,
-                            payload: null}
-    let token = req.body.webtoken
-    let isValid;
-    let payload;
-    try {
-        let decoded = jwt.verify(token, priv.signature, {"alg": "HS256", "typ": "JWT"});
-        isValid = true;
-        req.user = decoded.payload;
-        responseObject.payload = payload;
-    } catch (err) {
-        isValid = false;
-    }
-    //creates a new property for the request object, called user
-    if (isValid) {
-        responseObject.response = "Logged in";
-        res.send(responseObject);
-    } else {
-        responseObject.response = "Invalid login";
-        res.send(responseObject);
-    }
-}
-
-
-let createToken = (req, res) => {
-    let credentials = req.body;
-    console.log(req.body);
-    // let password = credentials.password;
-    // let id = credentials.id;
-    // let username = credentials.username;
-    // console.log(credentials);
-
-    // dbq.usernameLogin(username, password)
-    //     .then(results => {
-    //         console.log(results);
-    //         if (results.password === password && results.username === username) {
-    //             console.log("im here");
-    //             let token = jwt.sign(
-    //                 {name: results.username,
-    //                 userid: results.id},
-    //                 priv.signature,
-    //                 {expiresIn: '7d'})
-    //                 console.log(token);
-    //                 res.end(JSON.stringify(token));
-    //         } else {
-    //             res.end("Sorry, invalid login");
-    //         }
-    //     }).catch(error=> res.send({response: "bad login"}));
+    console.log(req.body)
+    dbq.createUser(req.body.username, req.body.password, req.body.email)  
+        // .then(data => {
+        //     let token = jwt.sign({id: data.id},
+        //         JwtPassword,
+        //         {expiresIn: '7d'});
+        //     res.send({ token });
+        //})
+        .catch(err => {
+            console.log(err);
+            res.send({ error: err });
+        });
     };
+        
+let userLogin = (req, res) => {
+    dbq.usernameLogin(req.body.username, req.body.password)
+        .then(data => {
+            res.send(data);
 
+        }).catch(error=> res.send({response: "bad login"}));
+};
 
-ex.post('/checktoken', validateToken);
-ex.post('/login', createToken);
+ex.post('/login', userLogin);
 ex.post('/register', newUser);
 
 ex.listen(3000);
