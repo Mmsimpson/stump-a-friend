61d98fb82fc4890d205f9e72ca8725f3ebf1911e
Mmsimpson <Marshall1044@gmail.com>
2018-09-28 14:30:37 -0400

    added some images to the quiz links

--- client/src/user-dashboard.js
+++ client/src/user-dashboard.js
@@ -10,13 +10,19 @@ let UserDashboardScreen = (props) => (
         <div className='user-dashboard'>
             <h3>Greetings: {props.user.userName}</h3>
             <Link to='/quizzes/new'>
-                <div className='dashboard-link'>Create Quiz</div>
+                <div className='dashboard-link'>Create Quiz
+                    <img src="http://blogs.edweek.org/edweek/finding_common_ground/Question%20Mark%20-%20Tumisu.jpg"></img>
+                </div>
             </Link>
             <Link to={`/users/${props.user.id}/quizzes/created`}>
-                <div className='dashboard-link'>Quizzes Created</div>
+                <div className='dashboard-link'>Quizzes Created
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCni3UDaBBz8RbJwcCj5SsCymB7Ydd-gD8y8FPrJCBDNvR6ZaO"></img>
+                </div>
             </Link>
             <Link to={`/users/${props.user.id}/quizzes/recieved`}>
-                <div className='dashboard-link'>Quizzes Received</div>
+                <div className='dashboard-link'>Quizzes Received
+                    <img src="https://steamcdn-a.akamaihd.net/steam/apps/425770/header.jpg?t=1533207997"></img>
+                </div>
             </Link>
         </div>
     </div>
